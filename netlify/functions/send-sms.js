80de211c6b0126d3f10123c9665df09414e3d696
andresslack <andresslack@hotmail.com>
2026-05-15 12:09:59 -0400

    Implement SMS sending functionality with Twilio
    Added a handler for sending SMS using Twilio API.

--- netlify/functions/send-sms.js
+++ netlify/functions/send-sms.js
@@ -1,1 +1,39 @@
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' };
+    }
 
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken  = process.env.TWILIO_AUTH_TOKEN;
+    const fromNumber = process.env.TWILIO_PHONE_NUMBER;
+
+    try {
+        const { to, message } = JSON.parse(event.body);
+
+        const response = await fetch(
+            `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`,
+            {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Basic ' + Buffer.from(`${accountSid}:${authToken}`).toString('base64'),
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new URLSearchParams({
+                    To:   to,
+                    From: fromNumber,
+                    Body: message
+                }).toString()
+            }
+        );
+
+        const data = await response.json();
+
+        if (response.ok) {
+            return { statusCode: 200, body: JSON.stringify({ success: true, sid: data.sid }) };
+        } else {
+            return { statusCode: 400, body: JSON.stringify({ error: data.message }) };
+        }
+    } catch (err) {
+        return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
+    }
+};
